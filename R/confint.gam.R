ba6940512645b5f9355fcc3310bbc1194f4b4c5c
Sam Clifford <samuel.clifford@qut.edu.au>
2017-05-02 18:12:43 +1000

    importing pipe

--- R/confint.gam.R
+++ R/confint.gam.R
@@ -4,7 +4,7 @@
 #' Computes confidence intervals for one or more parameters in a fitted model.
 #' This is a method specific to the \code{"gam"} class from package
 #' \code{"mgcv"}.
-#'
+#' @importFrom magrittr %>%
 #' @param object a fitted model object of class \code{"gam"}.
 #' @param parm a specification of which parameters are to be given confidence
 #' intervals, either a vector of numbers or a vector of names. If missing, all
@@ -21,7 +21,7 @@
 #' library(mgcv)
 #' dat <- data.frame(x = runif(n=100),
 #'                   y = runif(n=100)) %>%
-#'   mutate(z = rnorm(n=100,
+#'   dplyr::mutate(z = rnorm(n=100,
 #'                    mean = 1 - 2*x - sin(2*pi*y),
 #'                    sd = 0.1))
 #'
@@ -33,25 +33,25 @@
 confint.gam <- function(object, parm = NULL, level = 0.95) {
   # a method for extracting confidence intervals and returning a tidy data frame
 
-  mod.s <- summary.gam(object)
+  obj.s <- summary.gam(object)
 
-  E <- data.frame(Estimate = mod.s$p.coeff) %>%
-    mutate(Term = row.names(.)) %>%
-    select(Term, Estimate)
+  E <- data.frame(Estimate = obj.s$p.coeff) %>%
+    dplyr::mutate(Term = row.names(.)) %>%
+    dplyr::select(Term, Estimate)
 
-  SE <- data.frame(SE = mod.s$se) %>%
-    mutate(Term = row.names(.)) %>%
-    select(Term, SE)
+  SE <- data.frame(SE = obj.s$se) %>%
+    dplyr::mutate(Term = row.names(.)) %>%
+    dplyr::select(Term, SE)
 
   if (is.null(parm)){
     parm <- E$Term
   }
 
-  nu <- mod.s$residual.df
+  nu <- obj.s$residual.df
 
-  inner_join(E, SE) %>%
-    filter(Term %in% parm) %>%
-    mutate(L = Estimate +
+  dplyr::inner_join(E, SE) %>%
+    dplyr::filter(Term %in% parm) %>%
+    dplyr::mutate(L = Estimate +
              SE * qt(df = nu,
                      p = (1 - level) / 2),
            U = Estimate +
