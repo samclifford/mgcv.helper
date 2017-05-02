006098a0b5d53d67f4605fa59527dc497d30f3da
Sam Clifford <samuel.clifford@qut.edu.au>
2017-05-02 18:07:31 +1000

    adding R code

---
+++ R/confint.gam.R
@@ -0,0 +1,62 @@
+
+#' Confidence Intervals for Model Parameters
+#'
+#' Computes confidence intervals for one or more parameters in a fitted model.
+#' This is a method specific to the \code{"gam"} class from package
+#' \code{"mgcv"}.
+#'
+#' @param object a fitted model object of class \code{"gam"}.
+#' @param parm a specification of which parameters are to be given confidence
+#' intervals, either a vector of numbers or a vector of names. If missing, all
+#' parameters are considered.
+#' @param level the confidence level required.
+#'
+#' @return A tidy data frame containing parameter names, estimates and
+#' confidence intervals for parametric terms
+#' @export
+#'
+#' @examples
+#' set.seed(101)
+#' library(dplyr)
+#' library(mgcv)
+#' dat <- data.frame(x = runif(n=100),
+#'                   y = runif(n=100)) %>%
+#'   mutate(z = rnorm(n=100,
+#'                    mean = 1 - 2*x - sin(2*pi*y),
+#'                    sd = 0.1))
+#'
+#' fit1 <- gam(data=dat, z ~ y + s(x))
+#'
+#' confint(fit1)
+#' confint(fit1, parm="y", level=0.8)
+#'
+confint.gam <- function(object, parm = NULL, level = 0.95) {
+  # a method for extracting confidence intervals and returning a tidy data frame
+
+  mod.s <- summary.gam(object)
+
+  E <- data.frame(Estimate = mod.s$p.coeff) %>%
+    mutate(Term = row.names(.)) %>%
+    select(Term, Estimate)
+
+  SE <- data.frame(SE = mod.s$se) %>%
+    mutate(Term = row.names(.)) %>%
+    select(Term, SE)
+
+  if (is.null(parm)){
+    parm <- E$Term
+  }
+
+  nu <- mod.s$residual.df
+
+  inner_join(E, SE) %>%
+    filter(Term %in% parm) %>%
+    mutate(L = Estimate +
+             SE * qt(df = nu,
+                     p = (1 - level) / 2),
+           U = Estimate +
+             SE * qt(df = nu,
+                     p = 1 - (1 - level) / 2)) %>%
+    return
+
+}
---
+++ R/vif.gam.R
@@ -0,0 +1,55 @@
+#' Variance Inflation Factor
+#'
+#' This function takes a fitted mgcv model object and returns a data frame of
+#' variance inflation factors
+#'
+#' @param object An object of class gam
+#'
+#' @return VIF.df A data frame consisting of the VIF values for each parametric
+#' term in a fitted Generalised Additive Model
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(mgcv)
+#' library(dplyr)
+#'
+#' set.seed(101)
+
+#' N <- 100
+#' x1 <- runif(n=N)
+#' x2 <- runif(n=N)
+#' x3 <- runif(n=N) + 0.9*x1 - 1.75*x2
+#'
+#' df <- data.frame(x1 = x1,
+#'                  x2 = x2,
+#'                  x3 = x3) %>%
+#'   mutate(y = rnorm(n=N,
+#'                    mean = 1 - 2*x1 + 3*x2 - 0.5*x3,
+#'                    sd = 0.5))
+#'
+#' fit1 <- gam(data=df, y ~ x1 + x2 + x3)
+#'
+#' summary(fit1)
+#'
+#' vif.gam(fit1)
+#'
+vif.gam <- function(object){
+
+  obj.sum <- summary(object)
+  s2 <- mod$sig2 # estimate of standard deviation of residuals
+  X <- mod$model # data used to fit the model
+  n <- nrow(X) # how many observations were used in fitting?
+  v <- -1 # omit the intercept term, it can't inflate variance
+  varbeta <- obj.sum$p.table[v,2]^2 # variance in estimates
+  varXj <- apply(X=X[,row.names(obj.sum$p.table)[v]],MARGIN=2, var) # variance of all the explanatory variables
+  VIF <- varbeta/(s2/(n-1)*1/varXj) # the variance inflation factor, obtained by rearranging
+  # var(beta_j) = s^2/(n-1) * 1/var(X_j) * VIF_j
+
+  VIF.df <- data.frame(variable=names(VIF),
+                       vif=VIF,
+                       row.names=NULL)
+
+  return(VIF.df)
+}
